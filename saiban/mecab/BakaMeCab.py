93d58c91a48364925f8fd62844161617c1b63456
Artiom Basenko <demi.log@gmail.com>
2014-06-09 20:03:05 +0400

    Simplistic wrapper for mecab tool.

---
+++ saiban/mecab/BakaMeCab.py
@@ -0,0 +1,87 @@
+# encoding: utf-8
+# from __future__ import unicode_literals
+
+import subprocess
+
+
+class BakaMeCab:
+    """
+    Simplistic MeCab wrapper.
+    Calls mecab analyzer from system shell,
+    then parses result into list or dictionary.
+    """
+
+    ###############
+    # API methods #
+    ###############
+
+    def __init__(self, sentence):
+        """Initialize simple MeCab wrapper"""
+        self.command = u"echo %s | mecab"
+        self.sentence = sentence
+        self.words = []
+        self.info = {}
+
+    def get_words(self):
+        """Parse sentence and return list of words"""
+        return self.mecab().words
+
+    def get_info(self):
+        """Parse sentence into dictionary of words and corresponding info"""
+        return self.mecab().info
+
+    def get_readings(self):
+        """Return only list of readings corresponding to list of words"""
+        self.mecab()
+        return [self.info[w].pop() for w in self.info]
+
+    ###################
+    # Utility methods #
+    ###################
+
+    def mecab(self):
+        """Call mecab process"""
+        self.process(
+            self.call(
+                self.command % self.sentence
+            )
+        )
+        return self
+
+    def process(self, results):
+        """Process results, line by line"""
+        # Word list
+        for line in results:
+            if 'EOS' not in line:
+                self.words.append(line.split('	').pop(0))
+
+        # Words info
+        for line in results:
+            if 'EOS' not in line:
+                word, info = line.split('	')
+                self.info[word] = [
+                    i.strip() for i in info.split(',') if i != '*'
+                ]
+
+    def call(self, command):
+        """Execute shell command"""
+        try:
+            return subprocess.Popen(
+                command, shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT
+            ).stdout.readlines()
+        except OSError:
+            return []
+
+if __name__ == '__main__':
+    parser = BakaMeCab(u'任意のディレクトリ内で')
+
+    for word, info in parser.get_info().iteritems():
+        print word
+        for item in info:
+            print item
+        print '***'
+
+    for reading in parser.get_readings():
+        print reading
